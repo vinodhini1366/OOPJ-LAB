90a3869b0d07b98a1e7c39664cc154cfd9724bd0
vinodhini1366 <dandavanivinodhini@gmail.com>
2026-02-25 12:39:23 +0530

    Add files via upload

---
+++ MultipleInheritanceusingfinal.java
@@ -0,0 +1,59 @@
+final class student
+{
+int rollNumber;
+void getNumber(int n)
+	{
+	  rollNumber = n;
+} 
+ void printNumber()
+	{
+	 System.out.println("Roll Number: " + rollNumber);
+	}
+}
+class test extends student 
+{
+	float part1, part2;
+	void getMarks(float a, float b)
+	{
+		part1 = a;
+		part2 = b;
+}
+ void putMarks()
+{
+	 System.out.println("Marks in part1: " + part1);
+	 System.out.println("Marks in part2: " + part2);
+}
+}
+ interface sports
+ {
+	 float sportwt = 6.0f;
+	 void putwt();
+ }
+ class results extends test implements sports
+ {
+	 float total;
+	 public void putwt()
+	 {
+		 System.out.println("Sports Marks: " + sportwt);
+ }
+  void display()
+	 {
+	  total = part1 + part2 + sportwt;
+	  System.out.println("Total Marks: " + total);
+	  System.out.println("Roll Number: " + rollNumber);
+	 }
+ }
+  public class MultipleInheritanceusingfinal
+  {
+	public static void main(String[] args) 
+	{
+		results a = new results();
+		a.getNumber(101);
+		a.printNumber();
+		a.getMarks(85.5f,90.0f);
+		a.putMarks();
+		a.putwt();
+		a.display();
+	}
+}
+
