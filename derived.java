0183f0e23535d1d6e4c036f741ea375ce61c62fa
vinodhini1366 <dandavanivinodhini@gmail.com>
2026-03-07 18:38:34 +0530

    Add files via upload

---
+++ derived.java
@@ -0,0 +1,11 @@
+package p1;
+public class Derived extends protection
+{
+	public Derived(){
+	System.out.println("Dervied constructor");
+	System.out.println("n="+n);
+	//System.out.println("n_pri"+n_pri);
+	System.out.println("n_pro"+n_pro);
+	System.out.println("a_pub"+a_pub);
+	}
+}
