d80bb339f482b785ca9e83415a720f084718b0fb
vinodhini1366 <dandavanivinodhini@gmail.com>
2026-03-07 18:38:19 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,11 @@
+import p2.protection2;
+import p2.otherpackage;
+
+class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		protection2 ob1=new protection2();
+		otherpackage ob2 =new otherpackage();
+	}
+}
