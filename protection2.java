e1f5261e8cd24064cbc7a7e6b1d61ff1a66ce253
vinodhini1366 <dandavanivinodhini@gmail.com>
2026-03-07 18:40:01 +0530

    Add files via upload

---
+++ protection2.java
@@ -0,0 +1,9 @@
+package p2;
+public class protection2 extends p1.protection
+{
+	public protection2(){
+		System.out.println("derived other package constructor");
+			System.out.println("n_pro"+n_pro);
+			System.out.println("a_pub"+a_pub);
+	}
+}
